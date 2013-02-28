87e1bc8ef2b859a8deee764a4d6e136e1835e8f3
Martin Øinæs Myrseth <myrseth@gmail.com>
2013-02-28 16:30:47 +0100

    Override default constructor

--- src/backbonejs-computed.js
+++ src/backbonejs-computed.js
@@ -6,12 +6,21 @@
 
   Backbone.Model.extend = function ( properties, classProperties ) {
     var prop, action, propSpec, deps;
+    var parent = this;
 
+    properties = properties || {};
     var computedProps = properties.properties;
     delete properties.properties;
 
+    // Override Boostrap's default constructor
+    properties.constructor = function () {
+      return parent.apply( this, arguments );
+    };
+
+    // Extend our class from Boostrap.Model
     var Class = bbExtend.call( this, properties, classProperties );
 
+    // Setup computed properties
     if ( computedProps ) {
       for ( prop in computedProps ) {
         if ( computedProps.hasOwnProperty( prop ) ) {
@@ -31,6 +40,7 @@
       }
     }
 
+    // Override Bootstrap's default getter
     var get = Class.prototype.get;
     Class.prototype.get = function ( attr ) {
       if ( computedProps[ attr ] ) {
@@ -39,6 +49,7 @@
       return get.apply( this, arguments );
     };
 
+    // Override Bootstrap's default setter
     var set = Class.prototype.set;
     Class.prototype.set = function ( attr, value ) {
       if ( computedProps[ attr ] ) {
